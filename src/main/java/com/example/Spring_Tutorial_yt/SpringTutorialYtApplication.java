f6d4144bb4c7db1273cd27c6f824348008c76a01
Wilson Chavesta Gonzales <wilson_13_10@hotmail.com>
2022-11-16 23:52:07 -0500

    mi commit

---
+++ src/main/java/com/example/Spring_Tutorial_yt/SpringTutorialYtApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Tutorial_yt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTutorialYtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTutorialYtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Spring_Tutorial_yt/SpringTutorialYtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Tutorial_yt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTutorialYtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
